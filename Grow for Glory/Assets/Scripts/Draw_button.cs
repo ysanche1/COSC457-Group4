9a55da28a0fb362f0cf82f43e53de3a98b7c71e7
AlbertGarrow <agarrow1@emich.edu>
2021-11-17 12:29:36 -0500

    impl: DrawCards button

--- Grow for Glory/Assets/Scripts/Draw_button.cs
+++ Grow for Glory/Assets/Scripts/Draw_button.cs
@@ -4,7 +4,19 @@ using UnityEngine;
 
 public class Draw_button : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Card1;
+    public GameObject Card2;
+    public GameObject PlayerArea;
+
+    public void OnClick() 
+    {
+        for (int i = 0; i < 5; i++)
+        {
+            GameObject card = Instantiate(Card1, new Vector2(0, 0), Quaternion.identity); //Instantiate creates the game object in the scene
+            card.transform.SetParent(PlayerArea.transform, false);
+        }
+    }
+
     void Start()
     {
         
