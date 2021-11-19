df13c9742239d2b9bf9619f03608c5db8345a0c9
ysanche1 <ysanche1@emich.edu>
2021-11-19 18:46:50 -0500

    revert back to last working version

---
+++ Grow for Glory/Assets/Scripts/DragDrop.cs
@@ -0,0 +1,112 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragDrop : MonoBehaviour
+{
+    public GameObject Canvas;
+    public GameObject Button;
+    private GameObject DropZone1, DropZone2, DropZone3, DropZone4;
+
+    private bool isDragging = false;
+    private GameObject startParent;
+    private Vector2 startPosition;
+    private GameObject dropZone;
+    private bool isOverDropZone;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Canvas = GameObject.Find("Main_Canvas");
+        DropZone1 = GameObject.Find("Plot_1_area");
+        DropZone2 = GameObject.Find("Plot_2_area");
+        DropZone3 = GameObject.Find("Plot_3_area");
+        DropZone4 = GameObject.Find("Plot_4_area");
+        Button = GameObject.Find("Draw_button");
+
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        isOverDropZone = true;
+        dropZone = collision.gameObject;
+        if (dropZone == DropZone1)
+        {
+            Debug.Log("plot 1");
+        }
+        else if (dropZone == DropZone2)
+        {
+            Debug.Log("plot 2");
+        }
+        else if (dropZone == DropZone3)
+        {
+            Debug.Log("plot 3");
+        }
+        else if (dropZone == DropZone4)
+        {
+            Debug.Log("plot 4");
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        isOverDropZone = false;
+        dropZone = null;
+    }
+
+    public void StartDrag()
+    {
+        isDragging = true;
+        startParent = transform.parent.gameObject;
+        startPosition = transform.position;
+    }
+
+    public void EndDrag()
+    {
+        isDragging = false;
+        if (isOverDropZone)
+        {
+
+            if (dropZone == DropZone1)
+            {
+                Debug.Log("plot 1 +1");
+
+            }
+            else if (dropZone == DropZone2)
+            {
+                Debug.Log("plot 2 +1");
+
+            }
+            else if (dropZone == DropZone3)
+            {
+                Debug.Log("plot 3 +1");
+
+            }
+            else if (dropZone == DropZone4)
+            {
+                Debug.Log("plot 4 +1");
+
+            }
+            Button.GetComponent<Draw_button>().RemoveCardInHand();
+            Destroy(gameObject);
+
+        }
+        else
+        {
+            transform.position = startPosition;
+            transform.SetParent(startParent.transform, false);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (isDragging)
+        {
+            transform.position = new Vector2(Input.mousePosition.x, Input.mousePosition.y);
+            transform.SetParent(Canvas.transform, true);
+        }
+    }
+
+    
+}
